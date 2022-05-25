83b6a0821f3d0f963040477713224189a8cf317a
neiderp24 <neiderpb@gmail.com>
2022-05-25 18:19:16 -0500

    heroku finaly

--- utils/database.js
+++ utils/database.js
@@ -2,7 +2,7 @@ const { Sequelize } = require('sequelize');
 
 const dotenv = require('dotenv');
 
-dotenv.config({ path: ('./config.env')})
+dotenv.config({ path: ('./config.env') })
 
 const db = new Sequelize({
   dialect: 'postgres',
@@ -11,6 +11,14 @@ const db = new Sequelize({
   password: process.env.DB_PASSWORD,
   database: process.env.DB,
   logging: false,
+  dialectOptions:
+    process.env.NODE_ENV === 'production'
+      ? {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false
+        },
+      } : {},
 });
 
 module.exports = { db };
